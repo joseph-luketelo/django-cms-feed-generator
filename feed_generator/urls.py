bb155c54bb897d5fe027a50ba8ed28f144a22459
JOSEPH LUKETELO <joseph.luketelo@gmail.com>
2019-03-04 12:08:57 +0300

    Update urls.py

--- feed_generator/urls.py
+++ feed_generator/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import *
-from feed_generator.feeds import RSSFeed
 from django.conf.urls import include, url
 from feed_generator.feeds import RSSFeed
 from feed_generator.views import get_file
