709d893355e15ca1e969b9902a32da40e310b0e7
JOSEPH LUKETELO <joseph.luketelo@gmail.com>
2019-03-04 11:24:14 +0300

    Update urls.py

--- feed_generator/urls.py
+++ feed_generator/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import *
 from feed_generator.feeds import RSSFeed
 from django.conf.urls import include, url
+from django.urls import path
+from feed_generator.feeds import RSSFeed
+from feed_generator.views import get_file
 
-urlpatterns = url('feed_generator.feeds',
-    (r'^rss/$', RSSFeed()),
-)
-urlpatterns += url('feed_generator.views',
-    (r'^get_file/', 'get_file'),
-)
+path('rss/$', RSSFeed()),
+path('get_file/', get_file()),
+
